34a5a690bf620482a9d85078eece96a5be12279f
Stevenonmars <stevenonmars@aol.com>
2024-03-11 10:17:39 -0700

    prosperity log to excel.

---
+++ experiments/log_file_parser/python_file_csv_240311_1008.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+import json
+import pandas as pd
+
+path = Path('prosperity_2.log.txt')
+log_file = path.read_text()
+
+log_lines = log_file.splitlines()
+
+activites_log_start = log_lines.index('Activities log:')
+trade_history_start = log_lines.index('Trade History:')
+
+sandbox_logs_lines = log_lines[1:activites_log_start]
+activites_log_lines = log_lines[(activites_log_start + 1):trade_history_start]
+trade_history_lines = log_lines[(trade_history_start + 1):-1]
+
+def pop_json_from_list(log_lines):
+# read lines until close brace is found and remove them from the list and return the json string
+    json_string = ''
+    while log_lines:
+        line = log_lines.pop(0)
+        json_string += line
+        if line == '}':
+            break
+    return json_string
+
+sandbox_json_objects = []
+while sandbox_logs_lines:
+    json_string = pop_json_from_list(sandbox_logs_lines)
+    if json_string != '':
+        sandbox_json_objects += [json.loads(pop_json_from_list(sandbox_logs_lines))]
+
+# sandbox_json_objects[0].keys()
+# dict_keys(['sandboxLog', 'lambdaLog', 'timestamp'])
+
+trade_history_json_objects = json.loads(''.join(trade_history_lines) + ']')
+
+# trade_history_json_objects[0].keys()
+# dict_keys(['timestamp', 'buyer', 'seller', 'symbol', 'currency', 'price', 'quantity'])
+
+# json.loads(sandbox_json_objects[0])
+# {'sandboxLog': '', 'lambdaLog': 'traderData: \nObservations: (plainValueObservations: {}, conversionObservations: {})\nAcceptable price : 10\nBuy Order depth : 3, Sell order depth : 2\nSELL 1x 10002\nAcceptable price : 10\nBuy Order depth : 2, Sell order depth : 1\nSELL 1x 5002', 'timestamp': 0}
+
+sandbox_logs_header = ['sandboxLog', 'timestamp', 'lambdaLog']
+
+sandbox_logs_array = [sandbox_logs_header]
+for json_row in sandbox_json_objects:
+    row_array = []
+    for row_key in sandbox_logs_header:
+        row_array += [json_row[row_key]]
+    sandbox_logs_array += [row_array]
+
+activites_log_array = []
+for scsv_row in activites_log_lines:
+    activites_log_array += [scsv_row.split(';')]
+
+trade_history_header = list(trade_history_json_objects[0].keys())
+trade_history_array = [trade_history_header]
+for json_row in trade_history_json_objects:
+    row_array = []
+    for row_key in trade_history_header:
+        row_array += [json_row[row_key]]
+    trade_history_array += [row_array]
+
+# import pandas
+writer = pd.ExcelWriter('prosperity_log.xlsx', engine='xlsxwriter')
+
+sandbox_logs_df = pd.DataFrame.from_records(sandbox_logs_array)
+activites_log_df = pd.DataFrame.from_records(activites_log_array)
+trade_history_df = pd.DataFrame.from_records(trade_history_array)
+
+with pd.ExcelWriter('prosperity_log.xlsx') as writer:
+    sandbox_logs_df.to_excel(writer, sheet_name='Sandbox logs')
+    activites_log_df.to_excel(writer, sheet_name='Activites log')
+    trade_history_df.to_excel(writer, sheet_name='Trade History')
+# writer.close()
+
