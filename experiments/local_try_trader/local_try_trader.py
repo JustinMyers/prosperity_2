8af723499941b9b15339ccdc4c07149bd1ba0e8f
Stevenonmars <stevenonmars@aol.com>
2024-03-27 15:15:46 -0700

    cleaned up local_try_trader.

--- experiments/local_try_trader/local_try_trader.py
+++ experiments/local_try_trader/local_try_trader.py
@@ -1,75 +1,20 @@
 import argparse
 import importlib
 
-from pathlib import Path
-# import json
-# import pandas as pd
-
-# path = Path('prosperity_2.log.txt')
-# log_file = path.read_text()
-
 parser = argparse.ArgumentParser( description="getting file name from command line" )
 parser.add_argument("--trader_filename", required=True, type=str)
 args = parser.parse_args()
 trader_filename = args.trader_filename
 
 print("local_try_trader.py: trader_filename = ", trader_filename)
-# path = Path(trader_filename)
-
-# import subprocess
-# # subprocess.Popen('touch __init__.py', shell=True)
-# relative_path_to_trader = "/".join(trader_filename.split("/")[0:-1])
-# print("local_try_trader.py: relative_path_to_trader = ", relative_path_to_trader)
-
-# subprocess.Popen("touch " + relative_path_to_trader + "/__init__.py", shell=True)
-# # <Popen: returncode: None args: 'touch ../steven_demo_trader/__init__.py'>
-
-# # https://stackoverflow.com/questions/49039436/how-to-import-a-module-from-a-different-folder/49039555#49039555
-# import sys
-# import os
-
-# print("sys.path = ", sys.path)
-
-# # print("os.path = ", os.path)
-
-# sys.path.insert(0, os.path.abspath(relative_path_to_trader))
-
-# print("sys.path = ", sys.path)
-
-# # https://stackoverflow.com/questions/14071135/import-file-using-string-as-name
-# # demo_trader = __import__(trader_filename)
-
-# # # File "/home/stevenmyers/py_projects/prosperity_2/experiments/local_try_trader/local_try_trader.py", line 36, in <module>
-# # trader = __import__("/home/stevenmyers/py_projects/prosperity_2/experiments/local_try_trader/local_try_trader.py")
-
-# # from trader_filename import Trader
-# trader = __import__(trader_filename)
 
 trader = importlib.import_module(trader_filename, package=None)
 
 Trader = trader.Trader
 
-# from trader_filename import Trader
-
-# from demo_trader import Trader
 test_trader = Trader()
 
 import datamodel
-from datamodel import TradingState
-
-# sample_trading_state = {"listings": {"AMETHYSTS": {"denomination": 1, "product": "AMETHYSTS", "symbol": "AMETHYSTS"},
-#                                      "STARFRUIT": {"denomination": 1, "product": "STARFRUIT", "symbol": "STARFRUIT"}}, 
-#                         "market_trades": {}, 
-#                         "observations": {"conversionObservations": {}, "plainValueObservations": {}},
-#                         "order_depths": {"AMETHYSTS": {"buy_orders": {"9995": 29, "9996": 2, "10002": 1},
-#                                                        "sell_orders": {"10004": -2, "10005": -29}},
-#                                          "STARFRUIT": {"buy_orders": {"4997": 31, "5002": 1},
-#                                                        "sell_orders": {"5003": -31}}}, 
-#                         "own_trades": {},
-#                         "position": {},
-#                         "timestamp": 0,
-#                         "traderData": "SAMPLE"}
-
 from datamodel import Listing, OrderDepth, Trade, TradingState
 
 traderData = "SAMPLE"
@@ -113,11 +58,6 @@ order_depths = {
     "AMETHYSTS": amethysts_od,
     "STARFRUIT": starfruit_od
 }
-
-# order_depths = {"AMETHYSTS": OrderDepth({9995: 29, 9996: 2, 10002: 1},
-#                                         {10004: -2, 10005: -29}),
-#                 "STARFRUIT": OrderDepth({4997: 31, 5002: 1},
-#                                         {5003: -31})}
 
 own_trades = {'AMETHYSTS': [Trade(buyer= '', price= 10002.0, quantity= 1, seller= 'SUBMISSION', symbol= 'AMETHYSTS', timestamp= 0), 
                             Trade(buyer= '', price= 10002.0, quantity= 1, seller= 'SUBMISSION', symbol= 'AMETHYSTS', timestamp= 0)], 
